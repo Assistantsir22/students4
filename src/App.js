d97b5283985834de5bf5b6d0390424ee16133ba0
Assistantsir22 <122358254+Assistantsir22@users.noreply.github.com>
2023-05-10 13:19:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import AddEmployee from './components/AddEmployee';
+import {Route,Routes} from 'react-router-dom';
+import EmployeeList from './components/EmployeeList';
+import { BrowserRouter } from "react-router-dom";
+import Navbar from './components/Navbar';
+import UpdateEmployee from './components/UpdateEmployee';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route index element={<EmployeeList />} />
+          <Route path="/" element={<EmployeeList />}></Route>
+          <Route path="/employeeList" element={<EmployeeList />} />
+          <Route path="/addEmployee" element={<AddEmployee />} />
+          <Route path="/editEmployee/:id" element={<UpdateEmployee />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
